1a248df548ebed3452cb87fd70f70d4bcc71e5a0
root <bogingking@hotmail.com>
2019-03-17 14:44:05 -0400

    tentative of work

---
+++ Portfolio/js/jsGeneral.js
@@ -0,0 +1,16 @@
+
+
+function readMoreLess(el){
+        if($(el).hasClass("btn-success")){
+            $("#extraTxt").removeClass("collapse").addClass("collapse.show");
+            $(el).text("Read Less");
+            $(el).removeClass("btn-success").addClass("btn-primary");
+        }
+        else if($(el).hasClass("btn-primary")){
+            $("#extraTxt").removeClass("collapse.show").addClass("collapse");
+            $(el).text("Read More");
+            $(el).removeClass("btn-primary").addClass("btn-success");
+        }
+
+
+}
