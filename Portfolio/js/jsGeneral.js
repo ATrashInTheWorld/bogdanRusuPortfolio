ee4947f528ed7462b1f8ffaca113d2bb52fcdb15
root <bogingking@hotmail.com>
2019-03-23 15:55:29 -0400

    start scroll menu

--- Portfolio/js/jsGeneral.js
+++ Portfolio/js/jsGeneral.js
@@ -1,11 +1,20 @@
 
 $(document).ready(function() {
+
+    // set colors
     var colorsArr = ["#FF2D00", "#0093FF", "#00D203", "#DFDC00", "#FF00F5"];
     // red - blue - green - yellow - pink
     $("span.cTxt").each(function() {
         var randomNbr = Math.floor(Math.random() * 5);
         $(this).css("color", colorsArr[randomNbr]);
     });
+
+
+    //
+
+
+
+
 });
 
 
@@ -21,5 +30,9 @@ function readMoreLess(el){
             $(el).text("Read More");
             $(el).removeClass("btn-primary").addClass("btn-success");
         }
+}
+
+function menuAppear(){
+    console.log("OKOK");
 }
 
